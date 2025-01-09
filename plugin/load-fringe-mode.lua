2eacc4aa380ddae06ba6bc6a3efc61680f3bf2e0
Kyle Ronning <kyleronning+kronning6@gmail.com>
2025-01-08 18:41:07 -0600

    Make rename change not in last commit

--- plugin/load-fringe-mode.lua
+++ plugin/load-fringe-mode.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("FringeModeToggle", function()
-  require("fringe-mode").toggle_fringe_windows()
+  require("fringe-mode").toggle_fringe_mode_windows()
 end, {})
 
 vim.api.nvim_create_user_command("FringeModeStats", function()
